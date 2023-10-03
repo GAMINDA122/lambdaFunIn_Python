e2122fc6f8d293685179a93276a2187a226bbbe6
GAMINDA PREMASIRI <117364246+GAMINDA122@users.noreply.github.com>
2023-10-04 01:14:25 +0530

    Add files via upload

---
+++ lamda.py
@@ -0,0 +1,6 @@
+length = int(input("Enter your rectangular length : "))
+width = int(input("Enter your rectangular width : "))
+
+area = lambda length, width : length * width
+
+print(area(length, width))
